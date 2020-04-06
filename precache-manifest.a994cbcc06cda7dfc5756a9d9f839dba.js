73d0e578afa7ddf548fcb169e403e249818c8006
DruvaKumarbt <38590888+DruvaKumarbt@users.noreply.github.com>
2020-04-07 00:50:45 +0530

    Updates

---
+++ precache-manifest.a994cbcc06cda7dfc5756a9d9f839dba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e8781cb2090632768f7f897140f57d",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "d763b8e893be159c1b12",
+    "url": "/covid19/static/css/main.520856c3.chunk.css"
+  },
+  {
+    "revision": "741a51cd34b0822ec308",
+    "url": "/covid19/static/js/2.5f4e04de.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.5f4e04de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d763b8e893be159c1b12",
+    "url": "/covid19/static/js/main.571cb951.chunk.js"
+  },
+  {
+    "revision": "72a082ea0ecbed87a791",
+    "url": "/covid19/static/js/runtime-main.4701c998.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19/static/media/image.d7265326.png"
+  }
+]);
